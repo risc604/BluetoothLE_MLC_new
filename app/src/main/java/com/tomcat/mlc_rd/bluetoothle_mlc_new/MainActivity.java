bea6c0479698d7380dafc5542df535ec03add311
tomcat <tomcat.cheng@microlife.com.tw>
2016-04-07 16:19:04 +0800

    init new MLC Bluetooth LE test app.

---
+++ app/src/main/java/com/tomcat/mlc_rd/bluetoothle_mlc_new/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tomcat.mlc_rd.bluetoothle_mlc_new;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
