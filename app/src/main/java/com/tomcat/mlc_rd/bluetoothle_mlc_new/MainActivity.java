a305228da3744ef0ac0022fe03bde2b18617f25e
tomcat <tomcat.cheng@microlife.com.tw>
2016-04-07 17:18:06 +0800

    Bluetooth enble OK

--- app/src/main/java/com/tomcat/mlc_rd/bluetoothle_mlc_new/MainActivity.java
+++ app/src/main/java/com/tomcat/mlc_rd/bluetoothle_mlc_new/MainActivity.java
@@ -1,15 +1,120 @@
 package com.tomcat.mlc_rd.bluetoothle_mlc_new;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity
 {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private DeviceListAdapter   mLeDeviceListAdapter;
+    private BluetoothAdapter    mBluetoothAdapter;
+    private boolean             mScanning;
+    private Handler             mHandler;
+
+
+    private static final int    REQUEST_ENABLE_BT = 100;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE))
+        {
+            Toast.makeText(this, "BLE is not supported", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        final BluetoothManager  bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        if (mBluetoothAdapter == null)
+        {
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        //other initial
+    }
+
+    /**
+     * Dispatch onResume() to fragments.  Note that for better inter-operation
+     * with older versions of the platform, at the point of this call the
+     * fragments attached to the activity are <em>not</em> resumed.  This means
+     * that in some cases the previous state may still be saved, not allowing
+     * fragment transactions that modify the state.  To correctly interact
+     * with fragments in their proper state, you should instead override
+     * {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        if (!mBluetoothAdapter.isEnabled())
+        {
+            if (!mBluetoothAdapter.isEnabled())
+            {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+
+        Log.i(TAG, "onResume...");
+        mLeDeviceListAdapter = new DeviceListAdapter();
+        s
+
+    }
+
+    /**
+     * Dispatch incoming result to the correct fragment.
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "request Code: " + requestCode + "  result Code:" + resultCode);
+
+        /*
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_CANCELED)
+        {
+            finish();
+            return;
+        }
+        */
+
+        switch (requestCode)
+        {
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_CANCELED)
+                {
+                    finish();
+                    return;
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
+
